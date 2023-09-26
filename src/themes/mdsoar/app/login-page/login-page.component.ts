6db7131cec016e511efdc15d81f4717fe83d2071
David P. Steelman <dsteelma@umd.edu>
2023-09-26 11:00:39 -0400

    LIBCIR-341. Added "LoginPageComponent" to "mdsoar" theme
    Added "LoginPageComponent" to the "mdsoar" theme to enable overriding
    the DSpace logo with the MD-SOAR logo.
    
    https://umd-dit.atlassian.net/browse/LIBCIR-341

---
+++ src/themes/mdsoar/app/login-page/login-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { LoginPageComponent as BaseComponent } from '../../../../app/login-page/login-page.component';
+
+/**
+ * This component represents the login page
+ */
+@Component({
+  selector: 'ds-login-page',
+  // styleUrls: ['./login-page.component.scss'],
+  styleUrls: ['./login-page.component.scss'],
+  // templateUrl: './login-page.component.html'
+  templateUrl: './login-page.component.html'
+})
+export class LoginPageComponent extends BaseComponent {
+}
