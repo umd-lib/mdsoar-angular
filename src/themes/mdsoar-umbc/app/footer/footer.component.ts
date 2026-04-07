bc4ebef5c39fa0b6113e0d3bc99f085a21a5831c
David P. Steelman <dsteelma@umd.edu>
2026-04-07 12:07:49 -0400

    LIBCIR-479. Added accessibility information to institutional footers
    Added a "umd-footer-accessibility" component to the "mdsoar" theme as
    a common component for providing accessibility information in each of
    the footers in the institutional themes.
    
    Also moved the information about submitted a copyright complaint or
    withdrawal request into the component, to reduce duplication in the
    institutional footers.
    
    Updated the institutional footers to use the new component.
    
    https://umd-dit.atlassian.net/browse/LIBCIR-479

--- src/themes/mdsoar-umbc/app/footer/footer.component.ts
+++ src/themes/mdsoar-umbc/app/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import { FooterComponent as BaseComponent } from '../../../mdsoar/app/footer/footer.component';
+import { UmdFooterAccessibilityComponent } from '../../../mdsoar/app/umd-footer-accessibility/umd-footer-accessibility.component';
 
 @Component({
   selector: 'ds-themed-footer',
   styleUrls: ['../../../mdsoar/app/footer/footer.component.scss'],
   templateUrl: 'footer.component.html',
-  imports: [],
+  imports: [UmdFooterAccessibilityComponent],
   standalone: true,
 })
 export class FooterComponent extends BaseComponent {
---
+++ src/themes/mdsoar/app/umd-footer-accessibility/umd-footer-accessibility.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'ds-umd-footer-accessibility',
+  styleUrls: ['umd-footer-accessibility.component.scss'],
+  templateUrl: 'umd-footer-accessibility.component.html',
+  imports: [RouterLink],
+  standalone: true,
+})
+export class UmdFooterAccessibilityComponent {
+}
